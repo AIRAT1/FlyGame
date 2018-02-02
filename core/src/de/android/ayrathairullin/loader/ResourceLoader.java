b1e62a267c535aeb14916cb04d9a727689a2bf2c
Ayrat <ayrat1@mail.ru>
2018-02-02 10:20:12 +0300

    complete v1 09.00

--- core/src/de/android/ayrathairullin/loader/ResourceLoader.java
+++ core/src/de/android/ayrathairullin/loader/ResourceLoader.java
@@ -8,10 +8,11 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class ResourceLoader {
     private static TextureAtlas atlas;
-    private static Sprite logo, flyAndSpiders, background, grass, fly1, fly2, fly3, spider, webUp, webDown, playButtonUp, playButtonDown, ready, retry, gameOver, scoreboard, starOn, starOff, highScore;
+    public static Sprite logo, flyAndSpiders, background, grass, fly1, fly2, fly3, spider, webUp, webDown, playButtonUp, playButtonDown, ready, retry, gameOver, scoreboard, starOn, starOff, highScore;
     public static Animation flyAnimation;
     public static Sound dead, flap, coin, fall;
     public static Music fly;
@@ -20,23 +21,50 @@ public class ResourceLoader {
     public static void load() {
         atlas = new TextureAtlas(Gdx.files.internal("texture/texture.pack"), true);
         logo = new Sprite(atlas.findRegion("logo"));
-        playButtonUp = new Sprite(atlas.findRegion(""));
-        playButtonDown = new Sprite(atlas.findRegion(""));
-        ready = new Sprite(atlas.findRegion(""));
-        retry = new Sprite(atlas.findRegion(""));
-        gameOver = new Sprite(atlas.findRegion(""));
-        scoreboard = new Sprite(atlas.findRegion(""));
-        starOn = new Sprite(atlas.findRegion(""));
-        starOff = new Sprite(atlas.findRegion(""));
-        highScore = new Sprite(atlas.findRegion(""));
-        flyAndSpiders = new Sprite(atlas.findRegion(""));
-        background = new Sprite(atlas.findRegion(""));
-        grass = new Sprite(atlas.findRegion(""));
-        fly1 = new Sprite(atlas.findRegion(""));
-        fly2 = new Sprite(atlas.findRegion(""));
-        fly3 = new Sprite(atlas.findRegion(""));
-        spider = new Sprite(atlas.findRegion(""));
-        webUp = new Sprite(atlas.findRegion(""));
-        webDown = new Sprite(atlas.findRegion(""));
+        playButtonUp = new Sprite(atlas.findRegion("buttonOff"));
+        playButtonDown = new Sprite(atlas.findRegion("buttonOn"));
+        ready = new Sprite(atlas.findRegion("tapToFly"));
+        retry = new Sprite(atlas.findRegion("retry"));
+        gameOver = new Sprite(atlas.findRegion("gameOver"));
+        scoreboard = new Sprite(atlas.findRegion("wood"));
+        starOn = new Sprite(atlas.findRegion("starOn"));
+        starOff = new Sprite(atlas.findRegion("starOff"));
+        highScore = new Sprite(atlas.findRegion("highScore"));
+        flyAndSpiders = new Sprite(atlas.findRegion("flyAndSpiders"));
+        background = new Sprite(atlas.findRegion("background"));
+        grass = new Sprite(atlas.findRegion("grass"));
+        fly1 = new Sprite(atlas.findRegion("fly1"));
+        fly2 = new Sprite(atlas.findRegion("fly2"));
+        fly3 = new Sprite(atlas.findRegion("fly3"));
+        spider = new Sprite(atlas.findRegion("spider"));
+        webUp = new Sprite(atlas.findRegion("webUp"));
+        webDown = new Sprite(atlas.findRegion("webDown"));
+
+        TextureRegion[] fly = {fly1, fly2, fly3};
+        flyAnimation = new Animation(0.06f, fly);
+        flyAnimation.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+
+        dead = Gdx.audio.newSound(Gdx.files.internal("sounds/dead.wav"));
+        flap = Gdx.audio.newSound(Gdx.files.internal("sounds/flap.wav"));
+        coin = Gdx.audio.newSound(Gdx.files.internal("sounds/coin.wav"));
+        fall = Gdx.audio.newSound(Gdx.files.internal("sounds/fall.wav"));
+        ResourceLoader.fly = Gdx.audio.newMusic(Gdx.files.internal("sounds/fly.mp3"));
+
+        font = new BitmapFont(Gdx.files.internal("fonts/text.fnt"));
+        font.getData().setScale(.25f, -.25f);
+        whiteFont = new BitmapFont(Gdx.files.internal("fonts/whitetext.fnt"));
+        whiteFont.getData().setScale(.1f, -.1f);
+        shadow = new BitmapFont(Gdx.files.internal("fonts/shadow.fnt"));
+        shadow.getData().setScale(.25f, -.25f);
+    }
+
+    public static void dispose() {
+        atlas.dispose();
+        dead.dispose();
+        flap.dispose();
+        coin.dispose();
+        fly.dispose();
+        font.dispose();
+        shadow.dispose();
     }
 }
