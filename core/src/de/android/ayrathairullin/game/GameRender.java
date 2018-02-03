e7d2324611b1e4c13192eaab265442601000ab78
Ayrat <ayrat1@mail.ru>
2018-02-03 09:05:20 +0300

    complete v3 03.15

--- core/src/de/android/ayrathairullin/game/GameRender.java
+++ core/src/de/android/ayrathairullin/game/GameRender.java
@@ -2,8 +2,11 @@ package de.android.ayrathairullin.game;
 
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -15,60 +18,82 @@ public class GameRender {
     private int midPointY;
     private int midPointX;
 
-    private GameWorld mWorld;
+    private GameWorld world;
     private OrthographicCamera camera;
     private ShapeRenderer shapeRenderer;
-
     private SpriteBatch batch;
-
-    private Fly myFly;
-
+    private Fly fly;
+    private Sprite background, grass, flyMid, spider, webUp, webDown, ready, flyLogo, gameOver,
+            highScore, scoreboard, starOn, starOff, retry;
+    private Animation flyAnimation;
+    private Music music;
 
     public GameRender(GameWorld world, int gameHeight, int midPointY, int midPointX) {
-        mWorld = world;
-
+        this.world = world;
         this.midPointX = midPointX;
         this.midPointY = midPointY;
-
         camera = new OrthographicCamera();
         camera.setToOrtho(true, 136, gameHeight);
-
         batch = new SpriteBatch();
         batch.setProjectionMatrix(camera.combined);
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setProjectionMatrix(camera.combined);
+
+        initGameObjects();
+        initAssets();
     }
 
-    public void render(float delta, float runTime){
-
-        Fly fly = mWorld.getFly();
-
+    public void render(float delta, float runTime) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
         shapeRenderer.setColor(186 / 255.0f, 224 / 255.0f, 213 / 255.0f, 1);
         shapeRenderer.rect(0, 0, 136, midPointY + 66);
-
         shapeRenderer.setColor(167 / 255.0f, 211 / 255.0f, 152 / 255.0f, 1);
         shapeRenderer.rect(0, midPointY + 66, 136, 11);
-
         shapeRenderer.setColor(75 / 255.0f, 136 / 255.0f, 178 / 255.0f, 1);
         shapeRenderer.rect(0, midPointY + 77, 136, 53);
-
         shapeRenderer.end();
 
         batch.begin();
         batch.disableBlending();
-
-        batch.draw(ResourceLoader.background, 0, midPointY + 23, 136, 43);
-
+        batch.draw(background, 0, midPointY + 23, 136, 43);
         batch.enableBlending();
-
-        batch.draw((TextureRegion) ResourceLoader.flyAnimation.getKeyFrame(runTime),
-                fly.getX(), fly.getY(), fly.getWidth(), fly.getHeight());
-
+        drawFly(runTime);
         batch.end();
+    }
+
+    private void initAssets() {
+        background = ResourceLoader.background;
+        grass = ResourceLoader.grass;
+        flyAnimation = ResourceLoader.flyAnimation;
+        flyMid = ResourceLoader.fly2;
+        spider = ResourceLoader.spider;
+        webUp = ResourceLoader.webUp;
+        webDown = ResourceLoader.webDown;
+        ready = ResourceLoader.ready;
+        flyLogo = ResourceLoader.flyAndSpiders;
+        gameOver = ResourceLoader.gameOver;
+        highScore = ResourceLoader.highScore;
+        scoreboard = ResourceLoader.scoreboard;
+        retry = ResourceLoader.retry;
+        starOn = ResourceLoader.starOn;
+        starOff = ResourceLoader.starOff;
+        music = ResourceLoader.fly;
+    }
+
+    private void initGameObjects() {
+        fly = world.getFly();
+    }
+
+    private void drawFly(float runTime) {
+        if (fly.notFlap()) {
+            batch.draw(flyMid, fly.getX(), fly.getY(), fly.getWidth() / 2f, fly.getHeight() / 2f,
+                    fly.getWidth(), fly.getHeight(), 1, 1, fly.getRotation());
+        }else {
+            batch.draw((TextureRegion) flyAnimation.getKeyFrame(runTime), fly.getX(), fly.getY(), fly.getWidth() / 2f,
+                    fly.getHeight() / 2f, fly.getWidth(), fly.getHeight(), 1, 1, fly.getRotation());
+        }
     }
 }
